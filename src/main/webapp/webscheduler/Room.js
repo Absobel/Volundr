ba1aa41b5ebad81dc9a00f1998886be56af9f1ef
Jo.Blade <bladeur13@free.fr>
2024-05-25 19:12:06 +0200

    page pour ajouter des créneaux à l'événement

---
+++ src/main/webapp/webscheduler/Room.js
@@ -0,0 +1,216 @@
+/** convertir un string en nb de minutes en tenant compte de l'unité */
+function toTime(time) {
+  if (time.split('h').length > 1) {
+    return Number(time.split('h')[0]) * 60;
+  } else {
+    return Number(time);
+  }
+}
+
+
+/** pgcd de deux entiers */
+function gcd(num1, num2) {
+  if (num2 == 0)
+  return num1;
+  return gcd(num2, num1 % num2);
+}
+
+
+/** plus petit commun multiple d'un tableau */
+function lcm_of_array(arr) {
+  let lcm = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    let num1 = lcm;
+    let num2 = arr[i];
+    let gcd_val = gcd(num1, num2);
+    lcm = (lcm * arr[i]) / gcd_val;
+  }
+  return lcm;
+}
+
+
+export default class Room extends HTMLElement {
+
+  constructor () {
+    super();
+
+    this.cases = document.createElement('ul');
+    this.cases.classList.add('cases');
+
+    this.creneaux = document.createElement('ul');
+    this.creneaux.classList.add('creneaux');
+
+    /** Ne pas l'appeler dans le constructeur
+     * sinon le webcomponent creneau n'a pas le temps de bien charger
+    this.updateCreneauxList(); */
+  }
+
+  connectedCallback() {
+    this.scheduler = this.closest('volundr-scheduler');
+    this.scheduler.updateSallesList();
+
+    this.appendChild(this.cases);
+    this.appendChild(this.creneaux);
+  }
+
+  disconnectedCallback () {
+    if (this.scheduler !== null) {
+      this.scheduler.updateSallesList();
+    }
+  }
+
+  /** Nettoyer et actualiser la liste des créneaux quand un nouveau fils
+   * est ajouté ou supprimé */
+  updateCreneauxList () {
+
+    /* supprimer les li vides ou elements qui ont rien à faire là */
+    for (let element of this.creneaux.children) {
+
+      switch (element.tagName) {
+        case "LI":
+          if (element.childElementCount === 0) {
+            element.remove();
+          }
+          break;
+        case "VOLUNDR-CRENEAU":
+          let creneau = document.createElement('li');
+          creneau.appendChild(element);
+          this.creneaux.appendChild(creneau);
+          break;
+        default:
+          element.remove();
+          break;
+      }
+    }
+
+    /* ajouter les nouveaux créneaux à la liste des créneaux */
+    Array.from(this.children).forEach(element => {
+      if (element.tagName === "VOLUNDR-CRENEAU") {
+        let creneau = document.createElement('li');
+        creneau.appendChild(element);
+
+        /* le but est d'insérer en respectant l'ordre des temps de début */
+        let existing = null; /* l'élément avant lequel on doit insérer */
+        for (let el of this.creneaux.getElementsByTagName('volundr-creneau')) {
+          if (existing === null || (el.time < existing.time)) {
+            if (element.time < el.time) {
+              existing = el;
+            }
+          }
+        }
+
+        this.creneaux.insertBefore(creneau,
+          existing === null ? null : existing.parentElement);
+      }
+    });
+
+    /* mettre à jour tout le reste */
+    this.updateGridColumns();
+    Room.observedAttributes.map(x => this.setAttribute(x, this.getAttribute(x)))
+  }
+
+  static get observedAttributes() { return ['data-name', 'data-time-start', 'data-time-end']; }
+
+  /** L'heure du début du temps affiché pour placer des créneaux */
+  get timeStart() { return toTime(this.getAttribute('data-time-start')) }
+
+  /** L'heure de la fin du temps affiché pour placer des créneaux */
+  get timeEnd() { return toTime(this.getAttribute('data-time-end')) }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+
+    if (name === "data-name") {
+      if (this.closest('li') !== null) {
+        this.closest('li').getElementsByTagName('h3')[0].innerText = newValue;
+      }
+      return;
+    }
+
+
+    let timeEnd = this.getAttribute('data-time-end');
+    let timeStart = this.getAttribute('data-time-start');
+
+    if (newValue === null || timeStart === null || timeEnd === null) {
+      return;
+    }
+
+    /* on ne peut pas utiliser this.timeStart/End car récursion infinie */
+    let nbCases = toTime(timeEnd) - toTime(timeStart);
+
+    this.creneaux.style.gridTemplateRows = `repeat(${nbCases}, 1fr)`;
+
+    /* on change le nombre de balises li dans le tableau des cases.
+         * ça sert à l'affichage et à pouvoir sélectionner l'heure ou placer
+         * le créneau quand on passe la souris dessus */
+    let nbLi = Math.ceil(nbCases / 15);
+
+    /* On supprime tout */
+    while (this.cases.childElementCount > 0) {
+      this.cases.removeChild(this.cases.lastChild);
+    }
+
+    /* On rajoute ceux qui manquent */
+    while (this.cases.childElementCount < nbLi) {
+      let newLi = document.createElement('li');
+      let time = this.cases.childElementCount * 15 +
+        toTime(this.getAttribute('data-time-start'));
+
+      /* événement au survol, on peut le récupérer dans nos scripts
+           * via le tableau de fonctions de callback de l'objet Scheduler */
+      newLi.addEventListener("mousemove", () => {
+        if (this.scheduler.lastHover !== newLi) {
+          this.scheduler.caseHoverCallback.map(x => x(this, time));
+        }
+        this.scheduler.lastHover = newLi;
+      });
+
+      newLi.addEventListener("click", () => {
+        this.scheduler.caseClickCallback.map(x => x(this, time));
+      });
+
+      /* Ajouter le texte de l'heure */
+      if (time % 60 === 0) {
+        let hourSpan = document.createElement('span');
+        hourSpan.textContent = `${time / 60}h`;
+        newLi.appendChild(hourSpan);
+      }
+
+      this.cases.appendChild(newLi);
+    }
+  }
+
+  /** mettre le bon nombre de colonnes à la grille css des créneaux
+   * et mettre à jour les créneaux pour qu'ils aient la bonne largeur */
+  updateGridColumns () {
+    let timeStart = toTime(this.getAttribute('data-time-start'));
+    let liLen = Array(this.cases.childElementCount).fill(0);
+    let creneaux = Array.from(this.getElementsByTagName('volundr-creneau'));
+
+    /* calculer les entrelacement entre les créneaux */
+    creneaux.forEach(el => {
+      for (let i = el.time; i < (el.time + el.duration); i += 15) {
+        liLen[(i - timeStart) / 15]++;
+      }
+    });
+
+    /* Attention à bien retirer les valeurs nulles
+     * avant de calculer le lcm */
+    let nbColonnes = lcm_of_array(liLen.filter(x => x != 0));
+
+    /* changer le nombre de colonnes de la grille css */
+    this.creneaux.style.gridTemplateColumns = `repeat(${nbColonnes}, 1fr)`;
+
+    /* changer la largeur des créneaux dans l'affichage */
+    creneaux.forEach(el => {
+      let maxNbEntrelacement = 1;
+      for (let i = el.time; i < (el.time + el.duration); i += 15) {
+        let pos = (i - timeStart) / 15;
+        if (liLen[pos] > maxNbEntrelacement) {
+          maxNbEntrelacement = liLen[pos];
+        }
+      }
+
+      el.parentElement.style.gridColumn = `span ${nbColonnes / maxNbEntrelacement}`;
+    });
+  }
+}
---
+++ src/main/webapp/webscheduler/page_ajout_creneaux.js
@@ -0,0 +1,202 @@
+import Scheduler from "./Scheduler.js";
+import Room from "./Room.js";
+import Creneau from "./Creneau.js";
+
+customElements.define('volundr-scheduler', Scheduler);
+customElements.define('volundr-room', Room);
+customElements.define('volundr-creneau', Creneau);
+
+/* l'id de l'event qu'on modifie */
+const id = new URLSearchParams(window.location.search).get('id');
+
+const scheduler = document.getElementById('scheduler');
+const addCreneau = document.getElementById('add-creneau');
+const choixDuree = document.getElementById('duree-creneau');
+const choixSalle = document.getElementById('choix-salle');
+const addSalle = document.getElementById('add-salle');
+
+
+/** Fonction pour ajouter une salle dans le scheduler si elle n'y est pas déjà */
+function addSalleScheduler(idSalle, nomSalle) {
+  let idAttr = `volundr-room-${idSalle}`;
+
+  /* on crée la room seulement si elle n'est pas déjà présente */
+  if (document.getElementById(idAttr) === null) {
+    let new_room = document.createElement('volundr-room');
+    new_room.setAttribute('data-name', nomSalle);
+    new_room.setAttribute('id', idAttr);
+    new_room.setAttribute('data-id', idSalle);
+    new_room.setAttribute('data-time-start', "8h");
+    new_room.setAttribute('data-time-end', "18h");
+    scheduler.appendChild(new_room);
+  }
+}
+
+
+function updateAffichage() {
+  // Récupérer la liste de toutes les salles de la base de donnée pour pouvoir les selectionner
+  fetch(`http://localhost:8080/Volundr/rest/tutorial/getSalles`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      choixSalle.innerHTML = "";
+      data.map(currentValue => {
+        let newOption = document.createElement('option');
+        newOption.setAttribute('value', currentValue.id);
+        newOption.innerText = `${currentValue.batiment}.${currentValue.numero}`;
+        choixSalle.appendChild(newOption);
+      })
+    })
+
+
+  // Récupérer la liste des créneaux de l'event et les ajouter dans le scheduler
+  fetch(`http://localhost:8080/Volundr/rest/tutorial/getCasesEvent/${id}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      /* on supprime tous les anciens créneaux */
+      Array.from(scheduler.getElementsByTagName('volundr-creneau')).forEach(e => e.remove());
+
+      /* on ajoute tous les créneaux obtenus depuis le serveur */
+      data.map(creneau => {
+        /* on s'assure que la salle existe */
+        addSalleScheduler(creneau.salleC.id,
+          `${creneau.salleC.batiment}.${creneau.salleC.numero}`);
+
+        /* on crée l'objet 'volundr-creneau' et on l'ajoute au scheduler */
+        let new_creneau = new Creneau();
+        new_creneau.setAttribute('data-time', creneau.debutCreneau);
+        new_creneau.setAttribute('data-duration',
+          creneau.finCreneau - creneau.debutCreneau);
+
+        /* important de récupérer l'id sinon quand on va rappeler le serveur
+         * on va créer une copie au lieu de modifier l'original */
+        new_creneau.setAttribute('data-id', creneau.id);
+
+        document.getElementById(`volundr-room-${creneau.salleC.id}`).appendChild(new_creneau);
+
+
+        /* ON AJOUTE UNE FONCTION QUI SUPPRIME LE CRÉNEAU SI ON CLIQUE DESSUS */
+        new_creneau.addEventListener('click', () => supprimerCreneau(new_creneau));
+      });
+    });
+}
+
+
+/** Envoyer une requête au serveur pour supprimer le créneau passé en paramètre
+ * puis update l'affichage */
+function supprimerCreneau (creneauElement) {
+  // Appel à l'api pour ajouter une personne à la BDD
+  fetch(`http://localhost:8080/Volundr/rest/tutorial/delCasesEvent/${id}`, {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    /* seul l'id est utile à envoyer pour la suppression
+     * attention: c'est une liste qu'il faut envoyer même si un seul élément */
+    body: JSON.stringify([{ "id" : creneauElement.getAttribute('data-id') }])
+
+  }).then( (response) => { 
+      //do something awesome that makes the world a better place
+      console.log(response)
+
+      // Appel à l'api pour update l'affichage
+      updateAffichage();
+    });
+}
+
+
+/** Renvoie le JSON à envoyer pour la requête de création de créneaux */
+function addCreneauxReqJSON() {
+  return Array.from(scheduler.getElementsByTagName('volundr-room')).flatMap(room => {
+    let roomJSON = { "id" : room.getAttribute('data-id') };
+    return Array.from(room.getElementsByTagName('volundr-creneau'), c => {
+      let result = {
+        "debutCreneau" : c.time,
+        "finCreneau" : c.time + c.duration,
+        "salleC" : roomJSON
+      };
+
+      /* important de récupérer l'id sinon quand on va rappeler le serveur
+       * on va créer une copie au lieu de modifier l'original */
+      if (c.getAttribute('data-id')) {
+        result["id"] = c.getAttribute('data-id');
+      }
+      return result;
+    });
+  });
+}
+
+
+addSalle.addEventListener('click', () =>
+  addSalleScheduler(choixSalle.value, choixSalle.selectedOptions[0].text));
+
+
+addCreneau.addEventListener('click', () => {
+  /* n'executer la fonction que si on est pas déjà en train
+   * de placer un créneau */
+  if (scheduler.caseHoverCallback.length > 0) {
+    return;
+  }
+
+  /* pour placer le créneau, on veut cliquer sur les horaires et non sur
+   * le créneau lui-même */
+  scheduler.classList.remove("click-creneau");
+
+  let new_creneau = new Creneau();
+  new_creneau.setAttribute('data-duration', choixDuree.value);
+
+  scheduler.caseHoverCallback = [
+    ((room, time) => {
+      if ((time + new_creneau.duration) <= room.timeEnd) {
+        new_creneau.remove();
+        new_creneau.setAttribute('data-time', time);
+        room.appendChild(new_creneau);
+      }
+    })
+  ]
+
+  /* Quand on clique, on va envoyer une requête pour mettre à jour les créneaux
+   * sur le serveur puis recharger la liste des créneaux */
+  scheduler.caseClickCallback = [
+    ((room, time) => {
+      document.getElementsByTagName('volundr-scheduler')[0].caseClickCallback = [];
+      document.getElementsByTagName('volundr-scheduler')[0].caseHoverCallback = [];
+
+      /* on clique à nouveau sur les créneaux plutôt que sur les horaires */
+      scheduler.classList.add("click-creneau");
+
+      console.log(addCreneauxReqJSON());
+
+      // Appel à l'api pour ajouter une personne à la BDD
+      fetch(`http://localhost:8080/Volundr/rest/tutorial/addCasesEvent/${id}`, {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(addCreneauxReqJSON())
+
+      }).then( (response) => { 
+          //do something awesome that makes the world a better place
+          console.log(response)
+
+          // Appel à l'api pour update l'affichage
+          updateAffichage();
+        });
+    })
+  ]
+
+});
+
+// actualiser l'affichage au chargement de la page
+window.onload = updateAffichage;
